132828634a56160420118b685eb56916a6f6a523
mahdi hanafi <ehanafi@fpay-worldwide.com>
2019-10-25 16:47:10 +0100

    Minor fixes

--- src/android/HelloWorldFpay.java
+++ src/android/HelloWorldFpay.java
@@ -29,7 +29,7 @@ public class HelloWorldFpay extends CordovaPlugin {
             try {
                 int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                callback.success("" + (p1+p3));
+                callback.success("" + (p1+p2));
             } catch(Exception ex) {
                 callback.error("Someting went wrong " + ex);
             }
@@ -43,7 +43,7 @@ public class HelloWorldFpay extends CordovaPlugin {
             try {
                 int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                callback.success("" + (p1-p3));
+                callback.success("" + (p1-p2));
             } catch(Exception ex) {
                 callback.error("Someting went wrong " + ex);
             }
