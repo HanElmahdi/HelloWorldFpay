ee2886536a8b9137df340bde8ae927191aea8796
mahdi hanafi <ehanafi@fpay-worldwide.com>
2019-10-29 15:44:32 +0100

    remove tets java

--- src/android/HelloWorldFpay.java
+++ src/android/HelloWorldFpay.java
@@ -3,14 +3,6 @@ package cordova.plugin.helloworldfpay;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
-/*import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;*/
-//import package_1.NewClass;
-
-/**
- * This class echoes a string called from JavaScript.
- */
 public class HelloWorldFpay extends CordovaPlugin {
 
     @Override
@@ -44,11 +36,7 @@ public class HelloWorldFpay extends CordovaPlugin {
             try {
                 int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                //callback.success("" + (p1-p2));
-                Test t1 = new Test();
-                callback.success(t1.test());
-                //NewClass n = new NewClass();
-                //callback.success( n.test() );
+                callback.success("" + (p1-p2));
                 
             } catch(Exception ex) {
                 callback.error("Someting went wrong " + ex);
