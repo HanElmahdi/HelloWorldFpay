a889d29e8ec0005747ea8656d10af74c93aa7f79
mahdi hanafi <ehanafi@fpay-worldwide.com>
2020-01-27 17:29:45 +0100

    add new aar

--- src/android/PrintFpay.java
+++ src/android/PrintFpay.java
@@ -21,13 +21,29 @@ public class PrintFpay extends CordovaPlugin {
         return false;
     }
 
+    private void add(JSONArray args, CallbackContext callback) {
+        if(args != null) {
+            try {
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.success("" + (p1+p2));
+            } catch(Exception ex) {
+                callback.error("Someting went wrong " + ex);
+            }
+        } else {
+            callback.error("Please do not pass null value");
+        }
+    }
+
 	public void printText(JSONArray args, CallbackContext callback) {
-        BaseApp baseApp = (BaseApp) this.cordova.getActivity().getApplicationContext();
-        //String text = args.getJSONObject(0).getString("text");
+        //BaseApp baseApp = (BaseApp) this.cordova.getActivity().getApplicationContext();
+        BaseApp baseApp = (BaseApp) this.cordova.getActivity();
+
+        String text = args.getJSONObject(0).getString("text");
 
         if(args != null) {
             try {
-                String text = String.parseString(args.getJSONObject(0).getString("text"));
+                String text = args.getJSONObject(0).getString("text");
                 callback.success("" + (text));
                 
             } catch(Exception ex) {
@@ -37,7 +53,6 @@ public class PrintFpay extends CordovaPlugin {
             callback.error("Please do not pass null value");
         }
 
-        
         
         //baseApp.printText(text, false);
     }
