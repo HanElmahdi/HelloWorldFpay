d45d11fd27aa6bac7e3a752ba534aaedd8078c17
mahdi hanafi <ehanafi@fpay-worldwide.com>
2020-01-27 17:56:58 +0100

    add new aar

--- src/android/PrintFpay.java
+++ src/android/PrintFpay.java
@@ -21,25 +21,9 @@ public class PrintFpay extends CordovaPlugin {
         return false;
     }
 
-    private void add(JSONArray args, CallbackContext callback) {
-        if(args != null) {
-            try {
-                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                callback.success("" + (p1+p2));
-            } catch(Exception ex) {
-                callback.error("Someting went wrong " + ex);
-            }
-        } else {
-            callback.error("Please do not pass null value");
-        }
-    }
-
 	public void printText(JSONArray args, CallbackContext callback) {
         BaseApp baseApp = (BaseApp) this.cordova.getActivity().getApplicationContext();
         //BaseApp baseApp = (BaseApp) this.cordova.getActivity();
-
-        //String text = args.getJSONObject(0).getString("text");
 
         if(args != null) {
             try {
