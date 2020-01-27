a1552d9ecac0f38a4c6609eb8ec418218c6405ab
mahdi hanafi <ehanafi@fpay-worldwide.com>
2020-01-27 14:57:35 +0100

    add new aar

--- src/android/PrintFpay.java
+++ src/android/PrintFpay.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import android.app.Application;
 import ma.fpay.sunmilibrary.ionic.BaseApp;
 import ma.fpay.sunmilibrary.printerhelper.utils.AidlUtil;
+
 public class PrintFpay extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -21,7 +22,8 @@ public class PrintFpay extends CordovaPlugin {
     }
 
 	public void printText(JSONArray args, CallbackContext callback) {
-        BaseApp baseApp = (BaseApp) getApplication();
+        //BaseApp baseApp = (BaseApp) getApplication();
+        BaseApp baseApp = (BaseApp) this.cordova.getActivity().getApplicationContext();
 		String text = args.getJSONObject(0).getString("text");
         baseApp.printText(text, false);
     }
