64be55dbd64404fb9cc66ce3e2129d5e6c81c40b
HanElmahdi <mahdi.webdesigner@gmail.com>
2022-03-07 13:42:19 +0100

    Add new AAR 8

---
+++ src/android/BaseApp.java
@@ -0,0 +1,16 @@
+public class BaseApp /*extends Application*/ {
+
+    //@Override
+    public void onCreate() {
+        //super.onCreate();
+        //AidlUtil.getInstance().connectPrinterService(this);
+    }
+
+    public void printText(String content, boolean  isBluTooth){
+        //if (isBluTooth) {
+           // Printerhelper.getInstance().printByBluTooth(content);
+        //} else {
+            //Printerhelper.getInstance().printText(content);
+        //}
+    }
+}
