0aed4c813c75268a8e2b7ab271b8d2e0b1cc8921
HanElmahdi <mahdi.webdesigner@gmail.com>
2022-03-08 11:02:22 +0100

    Add new AAR 14

--- www/HelloWorldFpay.js
+++ www/HelloWorldFpay.js
@@ -3,6 +3,9 @@ var exec = require('cordova/exec');
 module.exports.test = function(success, error) {
     exec(success, error, 'HelloWorldFpay', 'test');
 };
+module.exports.test1 = function(success, error) {
+    exec(success, error, 'HelloWorldFpay', 'test1');
+};
 
 module.exports.add = function(arg0, success, error) {
     exec(success, error, 'HelloWorldFpay', 'add', [arg0]);
@@ -10,6 +13,10 @@ module.exports.add = function(arg0, success, error) {
 
 module.exports.substract = function(arg0, success, error) {
     exec(success, error, 'HelloWorldFpay', 'substract', [arg0]);
+};
+
+module.exports.printText = function(arg0, success, error) {
+    exec(success, error, 'HelloWorldFpay', 'printText', [arg0]);
 };
 
 module.exports.generateJSON = function(arg0, success, error) {
